6aaba81990b9c7e042a757c4732ac1bea13ae97a
Joshua Bechard <josh.bechard@gmail.com>
2014-06-08 06:33:42 +0000

    Customized the post page with a couple of test scenarios

--- server/post.js
+++ server/post.js
@@ -11,12 +11,25 @@ WebApp.connectHandlers
         Fiber(function() {
             
             var body = req.body;
-            console.log(body);
-            console.log(body.events[0].entity.card_holds[0].status)
+            console.log("body: " + body);
+            //calls within the code cannot assume that the body will contain certain parts. 
+            //first check what exists and then parse based on which kind of response is received. 
+            console.log(body.events[0].type);
+            
+          if (body.events[0].type === "bank_account.created") {
+//               console.log("status: " + body.events[0].entity.card_holds[0].status);
+//               console.log("card: " + body.events[0].entity.card_holds[0].links.card);
+            console.log("bank account: " + testApi.events[0].entity.bank_accounts[0].href);
+            console.log("bank account: " + testApi.events[0].entity.bank_accounts[0].id);
+            }
+          if(body.events[0].type === "account.create") {
+            console.log(testApi.events[0].entity.customers[0].href);
+            console.log(testApi.events[0].entity.customers[0].id);
+          }
             //var user = Meteor.users.findOne(userId);
  
             res.writeHead(200, {'Content-Type': 'application/json'});
-            //res.end(JSON.stringify(user.profile));
+            res.end(); //JSON.stringify(user.profile)
  
         }).run();
     });    
