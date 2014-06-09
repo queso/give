bf96c6049d8973d7ea7a8cbd87b1189fe44eae0f
Joshua Bechard <josh.bechard@gmail.com>
2014-06-09 22:40:24 +0000

    added variable to res.end and removed double calls

--- server/callbacks.js
+++ server/callbacks.js
@@ -20,41 +20,49 @@ WebApp.connectHandlers
           if (body.events[0].type === "bank_account.created") {
             console.log("bank account: " + body.events[0].entity.bank_accounts[0].href);
             console.log("bank account: " + body.events[0].entity.bank_accounts[0].id);
+            var sendToEnd = body.events[0].entity.bank_accounts[0].id;
             }
           if (body.events[0].type === "bank_account.updated") {
             console.log("bank account: " + body.events[0].entity.bank_accounts[0].href);
             console.log("bank account: " + body.events[0].entity.bank_accounts[0].id);
+            var sendToEnd = body.events[0].entity.bank_accounts[0].id;
           }
 
           //customer events
           if(body.events[0].type === "account.created") {
             console.log(body.events[0].entity.customers[0].href);
             console.log(body.events[0].entity.customers[0].id);
+            var sendToEnd = body.events[0].entity.customers[0].id;
           }
 
           //card events
           if(body.events[0].type === "card.created") {
             console.log(body.events[0].entity);
+            var sendToEnd = body.events[0].entity;
           }
 
           //charge events
           if(body.events[0].type === "debit.created") {
             console.log(body.events[0].entity.debits[0].href);
             console.log(body.events[0].entity.debits[0].id);
-          }
+            var sendToEnd = body.events[0].entity.debits[0].id;
+          } 
           if(body.events[0].type === "debit.succeeded") {
             console.log(body.events[0].entity.debits[0].href);
             console.log(body.events[0].entity.debits[0].id);
+            var sendToEnd = body.events[0].entity.debits[0].id;
           }
 
           //hold events
           if (body.events[0].type === "hold.created") {
             console.log("holds: " + body.events[0].entity.card_holds[0].href);
             console.log("holds: " + body.events[0].entity.card_holds[0].id);
+            var sendToEnd = body.events[0].entity.card_holds[0].id;
           }
           if (body.events[0].type === "hold.updated") {
             console.log("holds: " + body.events[0].entity.card_holds[0].href);
             console.log("holds: " + body.events[0].entity.card_holds[0].id);
+            var sendToEnd = body.events[0].entity.card_holds[0].id;
           }
           if ((body.events[0].type) === "hold.captured") {
             console.log("holds: " + body.events[0].entity.card_holds[0].href);
@@ -63,7 +71,12 @@ WebApp.connectHandlers
           }
  
             res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end(sendToEnd); //JSON.stringify(user.profile)
+            if (sendToEnd) {
+              res.end(sendToEnd); //JSON.stringify(user.profile)
+            } else {
+              res.end();
+            }
+
  
         }).run();
     });    
