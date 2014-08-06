28637c6dc872d67ac02eb123989f4f2c4dac5924
Joshua Bechard <josh.bechard@gmail.com>
2014-08-06 19:27:28 +0000

    Fixed the ? icon for Account type

--- client/views/donate/donation_form.js
+++ client/views/donate/donation_form.js
@@ -217,6 +217,12 @@ if(form.paymentInformation[0].donateWith === "card") {
       if(e.which === 17) { //17 is ctrl + q
         fillForm();
       }
+    },
+    'mouseover #accountTypeQuestion': function(e,tmpl) {
+      $('[name=checkGraphic]').toggle();
+    },
+    'click #accountTypeQuestion': function(e,tmpl) {
+      $('[name=checkGraphic]').toggle();
     }
 });
 
@@ -279,9 +285,7 @@ Template.DonationForm.rendered = function () {
   $('[name=city]').tooltip({container: 'body', trigger: 'hover focus', title: 'City', placement: 'auto top'});
   $('[name=region]').tooltip({container: 'body', trigger: 'hover focus', title: 'State/Region', placement: 'auto top'});
   $('[name=postal_code]').tooltip({container: 'body', trigger: 'hover focus', title: 'Postal Code', placement: 'auto top'});
-  $('[name=country]').tooltip({container: 'body', trigger: 'hover focus', title: 'Country', placement: 'auto top'});
-  $('#accountTypeQuestion').tooltip({container: 'body', trigger: 'hover focus', title: 'Test', placement: 'auto right', img: '/images/check_image.png'});
-  
+  $('[name=country]').tooltip({container: 'body', trigger: 'hover focus', title: 'Country', placement: 'auto top'});  
 
 //remove below before production 
 //Parsley form validation setup, commented to test other things while I wait to 
