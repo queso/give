eb40a04f515eba77f6ce9ca87cf4d5d74801b22f
Joshua Bechard <josh.bechard@gmail.com>
2014-09-29 07:42:13 +0000

    Changes

--- server/methods/orders.js
+++ server/methods/orders.js
@@ -74,13 +74,13 @@ _.extend(Utils, {
         }else{
             logger.info("Credit status was false or not set, starting to send out a credit.");
             var setModifier = { $set: {} };
-                setModifier.$set['recurring.transactions.' + transaction_guid + '.credit.sent'] = true;
+                setModifier.$set['recurring.transactions.' + transaction_guid + '.credit'] = {sent: true};
                 Donate.update({_id: id}, setModifier);
 
             var credit = Utils.extractFromPromise(balanced.get(Meteor.settings.bank_account_uri).credit({"appears_on_statement_as": name, "amount": amount}));
 
             var setModifierAgain = { $set: {} };
-                setModifierAgain.$set['recurring.transactions.' + transaction_guid + '.credit'] = {'amount': credit.amount, 'id': credit.id};
+                setModifierAgain.$set['recurring.transactions.' + transaction_guid + '.credit'] = {'amount': credit.amount, 'id': credit.id, 'sent': true};
                 Donate.update({_id: id}, setModifierAgain);
         }
             
