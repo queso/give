f53cef25b98e481bd4575caa23843f96810b2edc
Joshua Bechard <josh.bechard@gmail.com>
2014-10-16 14:50:20 +0000

    Fixes for credit in event_controller.js

--- server/methods/event_controller.js
+++ server/methods/event_controller.js
@@ -22,11 +22,11 @@ WebApp.connectHandlers.use(bodyParser.urlencoded({
                 logger.info('Body type: ' + body.events[0].type);
                 if (billy) {
                     logger.info("Billy Event received.");
-                    e.emit('start_billy', true, body);
+                    e.emit('start', true, body);
                     logger.info("This runs after Evts.recurring_controller call");
                 }else {
                     logger.info("Non-Billy Event received.");
-                    e.emit('start_billy', false, body);
+                    e.emit('start', false, body);
                     logger.info("This runs after Evts.recurring_controller call");
                 }
                 e.emit('end', body.events[0].type);
@@ -44,7 +44,7 @@ WebApp.connectHandlers.use(bodyParser.urlencoded({
                 });
                 res.end("Got it");//TODO: Remove Got it text, just leave blank when this is live.
             });
-            evt.on('start_billy', function (billy, body) {
+            evt.on('start', function (billy, body) {
                 if(billy){
                     var send_to_recurring = Evts.recurring_controller(body);
                 } else {
--- server/methods/one_time_event_controller.js
+++ server/methods/one_time_event_controller.js
@@ -1,10 +1,12 @@
 _.extend(Evts,{
 	one_time_controller: function(body) {
 		var type = Object.keys(body.events[0].entity)[0];
+		logger.info(type);
 		if(type === "debits"){
+			logger.info("Inside one_time_controller and debits area");
 			var status = 		body.events[0].entity[type][0].status;
 			var select_type = 	Evts.select_type(body);
-
+			logger.info(select_type);
 			if(select_type === "debit_created") {
 	 			var sending_email_for_created = Evts.send_received_email(false, body.events[0].entity.debits[0].id, null, status, body.events[0].entity.debits[0].amount);
 	 		}else if(select_type === "debit.succeeded") {
