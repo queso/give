49d48510eb831355faf6cacd51434f2d5be30bd5
Joshua Bechard <josh.bechard@gmail.com>
2014-10-16 15:10:47 +0000

    Fixes for credit in event_controller.js

--- server/methods/one_time_event_controller.js
+++ server/methods/one_time_event_controller.js
@@ -9,9 +9,9 @@ _.extend(Evts,{
 			logger.info(select_type);
 			if(select_type === "debit_created") {
 	 			var sending_email_for_created = Evts.send_received_email(false, body.events[0].entity.debits[0].id, null, status, body.events[0].entity.debits[0].amount);
-	 		}else if(select_type === "debit.succeeded") {
+	 		}else if(select_type === "debit_succeeded") {
 	 			var sending_email = Evts.send_email(false, body.events[0].entity.debits[0].id, null, status, body.events[0].entity.debits[0].amount);
-	 		}if(select_type === "debit.failed"){
+	 		}if(select_type === "debit_failed"){
 	 			var failed_update = Evts.failed_collection_update(false, type, body.events[0].entity.debits[0].id, null, body);
 	 		}
  		} else{
