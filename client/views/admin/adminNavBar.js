cbd9803925900b2fbb7bb8a2693e4bf052d01350
Joshua Bechard <josh@trashmountain.com>
2014-10-30 18:13:17 -0500

    Changed accounts redirect

--- client/views/admin/adminNavBar.js
+++ client/views/admin/adminNavBar.js
@@ -1,14 +1,14 @@
 Template.AdminNavBar.events({
 	'submit form': function(e, tmpl) {
 		//prevent the default reaction to submitting this form
-        e.preventDefault();
-        // Stop propagation prevents the form from being submitted more than once.
-        e.stopPropagation();
+                e.preventDefault();
+                // Stop propagation prevents the form from being submitted more than once.
+                e.stopPropagation();
 
-        console.log($('#admin_search').val());
-        Meteor.call('admin_search', $('#admin_search').val(), function (error, result) {
-        	result ? console.log(result): console.log(error);
-        });
+                console.log($('#admin_search').val());
+                Meteor.call('admin_search', $('#admin_search').val(), function (error, result) {
+                	result ? console.log(result): console.log(error);
+                });
 
 	} 
 });
